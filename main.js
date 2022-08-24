4328c0fac206c88a1b87ed8a94e37b74249c7422
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2022-08-24 14:29:00 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+
+var firstname="Zara";
+var lastname="Khan";
+console.log(firstname);
+console.log(lastname);
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var a = 10;
+var b = 20;
+var add = a+b;
+console.log(add);
