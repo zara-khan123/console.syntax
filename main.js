83e515189ab3861a9fefa9ef1d62132b6c5ec31c
zara-khan123 <106547199+zara-khan123@users.noreply.github.com>
2022-09-25 12:11:24 +0100

    Add files via upload

---
+++ main.js
@@ -0,0 +1,21 @@
+
+var firstname="Zara";
+var lastname="Khan";
+console.log(firstname);
+console.log(lastname);
+var fullname=firstname.concat(lastname);
+console.log(fullname);
+var a = 10;
+var b = 20;
+console.log(a);
+console.log(b);
+var add = a+b;
+console.log(add);
+var square_root=Math.sqrt(1024);
+console.log(square_root);
+var maxnumber=Math.max(876, 146, 347);
+console.log(maxnumber);
+var minnumber=Math.min(876, 146, 347);
+console.log(minnumber);
+var power=Math.pow(27, 5);
+console.log(power);
